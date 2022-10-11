b2ee36b6a343d0aa287027a54fc3f5e80736c962
Samuel Hyrum <samuelhymp@gmail.com>
2022-10-11 15:04:33 -0300

    Voltando ao normal

--- docker/todo-app/back-end/src/api/routes.js
+++ docker/todo-app/back-end/src/api/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-
 const routes = express();
 
 const tasksController = require('../controllers/Tasks');
